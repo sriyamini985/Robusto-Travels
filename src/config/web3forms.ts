0ef8cb26597df5e65f49e01369d86525f0f40c27
sriyamini985 <sriyamini659@gmail.com>
2026-08-21 10:10:41 +0530

    chore: Set active Web3Forms Access Key in configuration

--- src/config/web3forms.ts
+++ src/config/web3forms.ts
@@ -1,5 +1,5 @@
 export const WEB3FORMS_CONFIG = {
   // Register your email and get a free Access Key at: https://web3forms.com/
   // Replace the placeholder below with your key to start receiving live inquiries in your inbox!
-  accessKey: "YOUR_ACCESS_KEY_HERE"
+  accessKey: "a904bf2b-2d80-4f21-b2b2-f4934b96989c"
 };

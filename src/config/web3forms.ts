5d8f44eac2befd49698f2138b97c43d27922d501
sriyamini985 <sriyamini659@gmail.com>
2026-08-21 15:39:17 +0530

    chore: Set new Web3Forms Access Key for info.robustotravels@gmail.com

--- src/config/web3forms.ts
+++ src/config/web3forms.ts
@@ -1,5 +1,5 @@
 export const WEB3FORMS_CONFIG = {
   // Register your email and get a free Access Key at: https://web3forms.com/
   // Replace the placeholder below with your key to start receiving live inquiries in your inbox!
-  accessKey: "a904bf2b-2d80-4f21-b2b2-f4934b96989c"
+  accessKey: "e2061a72-3313-4c8b-a8ef-00d3c81830f4"
 };
